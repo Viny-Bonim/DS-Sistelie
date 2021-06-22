59406f7d94faf3892675d3ce9e7f502a8e1bb2b8
Gustavo dos Anjos Neri <gustavodosanjosneri@gmail.com>
2021-06-22 00:52:26 -0400

    Atualização da tela de Login
    Add: "Esqueceu sua senha?" e "Lembrar usuário?"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace DS_Sistelie
 {
@@ -23,6 +25,16 @@ namespace DS_Sistelie
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
