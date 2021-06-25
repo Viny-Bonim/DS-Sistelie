ef891523c3f7a45cb0887f5ccf2194c1e7bf2b9f
Gabriel <gabrielestevam010403@gmail.com>
2021-06-25 04:28:40 -0400

    Esboço Tela Cadastrar matéria - prima

---
+++ CadastrarMateriaPrima.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DS_Sistelie
+{
+    /// <summary>
+    /// Lógica interna para CadastrarMateriaPrima.xaml
+    /// </summary>
+    public partial class CadastrarMateriaPrima : Window
+    {
+        public CadastrarMateriaPrima()
+        {
+            InitializeComponent();
+        }
+    }
+}
