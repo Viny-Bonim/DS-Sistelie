7adc324baf9548791a008549cf529e89e2160bb8
Gustavo dos Anjos Neri <gustavodosanjosneri@gmail.com>
2021-06-24 22:31:14 -0400

    Adicionado a tela de cadastrar Cliente
    Ainda em produção

---
+++ CadastrarCliente.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DS_Sistelie
+{
+    /// <summary>
+    /// Lógica interna para CadastrarCliente.xaml
+    /// </summary>
+    public partial class CadastrarCliente : Window
+    {
+        public CadastrarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Salvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
