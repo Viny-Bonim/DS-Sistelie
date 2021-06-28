19ab34c5ec78ca6332fedb509a3421fe346f302a
Viny Bonim Scaldelai <vinyscaldelai@gmail>
2021-06-28 10:55:16 -0400

    Att botões de Consultar Despesas

--- Despesas/CadastrarDespesaWindow.xaml.cs
+++ Despesas/CadastrarDespesaWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace DS_Sistelie.Despesas
     /// </summary>
     public partial class CadastrarDespesaWindow : Window
     {
+        Despesas despesas = new Despesas();
+
         private List<string> grupoDespesa;
 
         public CadastrarDespesaWindow()
@@ -35,6 +37,43 @@ namespace DS_Sistelie.Despesas
             grupoDespesa.Add("Despesa Variável");
 
             cmbxGrupoDespesa.ItemsSource = grupoDespesa;
+        }
+
+        private void btnCancelarDespesa_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja Cancelar o Cadastro dessa Despesa?", "Cadastrar Despesa", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                MenuInicial menuInicial = new MenuInicial();
+                menuInicial.Show();
+                this.Close();
+            }
+        }
+
+        private void btnSalvarDespesa_Click(object sender, RoutedEventArgs e)
+        {
+            despesas.ValorDespesa = double.Parse(txtValorDespesa.Text);
+            despesas.dataDespesa = DateTime.Parse(dtpkDataDespesa.Text);
+            despesas.CodigoFornecedorCadDespesa = int.Parse(txtCodigoFornecedorDespesa.Text);
+            despesas.DescricaoDespesa = txtDescricaoDespesa.Text;
+            despesas.IdDespesa = int.Parse(txtIdDespesa.Text);
+            despesas.GrupoDespesa = cmbxGrupoDespesa.Text;
+
+            if (despesas.ValorDespesa.Equals("") && despesas.dataDespesa.Equals("")
+                && despesas.DescricaoDespesa.Equals("") && despesas.GrupoDespesa.Equals(""))
+            {
+                MessageBox.Show("Preencha todos os campos com *");
+            }
+            else
+            {
+                MessageBox.Show("Despesa cadastrada com sucesso!\n" +
+                    $"Valor da Despesa: {despesas.ValorDespesa}\n" +
+                    $"Data de Cadastro: {despesas.dataDespesa}\n" +
+                    $"Código do Forncedor: {despesas.CodigoFornecedorCadDespesa}\n" +
+                    $"Descrição da Despesa: {despesas.DescricaoDespesa}\n" +
+                    $"ID da Despesa: {despesas.IdDespesa}\n" +
+                    $"Grupo da Despesa: {despesas.GrupoDespesa}");
+            }
         }
     }
 }
