17af0bf4207324ef675dca1493e60960c7ff7907
Viny Bonim Scaldelai <vinyscaldelai@gmail.com>
2021-10-11 16:10:06 -0400

    Variáveis da Classe de conexão
    Variáveis da Classe de conexão

--- Database/Conexao.cs
+++ Database/Conexao.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace DS_Sistelie.Database
 {
     class Conexao
     {
+        private static string host = "localhost";
+
+        private static string port = "3306";
+
+        private static string user = "root";
+
+        private static string password = "root";
+
+        private static string dbname = "sistelie";
+
+        private static MySqlConnection connection;
+
+        private static MySqlCommand command;
     }
 }
