5de212ddfd556ee829947cfe3fb69a9a08a9c3c5
Viny Bonim Scaldelai <vinyscaldelai@gmail.com>
2021-10-11 19:50:38 -0400

    Introduzindo Interfaces para padronizar DAO

--- Models/FornecedorDAO.cs
+++ Models/FornecedorDAO.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DS_Sistelie.Interfaces;
 
 namespace DS_Sistelie.Models
 {
-    class FornecedorDAO
+    class FornecedorDAO : IDAO<Fornecedor>
     {
+        public void Delete(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fornecedor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fornecedor> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Fornecedor t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
