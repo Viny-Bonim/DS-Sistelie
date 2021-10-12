2bcc9c9d75b4d0a88dec95ac4c3b4aa10311afb3
Viny Bonim Scaldelai <vinyscaldelai@gmail.com>
2021-10-11 23:47:50 -0400

    Iniciando a parte de insert

--- Despesas/DespesasDAO.cs
+++ Despesas/DespesasDAO.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DS_Sistelie.Interfaces;
 
 namespace DS_Sistelie.Despesas
 {
-    class DespesasDAO
+    class DespesasDAO : IDAO<Despesas>
     {
+        public void Delete(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Despesas GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Despesas> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Despesas t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Models/FornecedorDAO.cs
+++ Models/FornecedorDAO.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DS_Sistelie.Interfaces;
+using DS_Sistelie.Database;
 
 namespace DS_Sistelie.Models
 {
     class FornecedorDAO : IDAO<Fornecedor>
     {
+        public static Conexao conn;
+
+        public FornecedorDAO()
+        {
+            conn = new Conexao();
+        }
+
         public void Delete(Fornecedor t)
         {
             throw new NotImplementedException();
